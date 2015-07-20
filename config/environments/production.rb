d50c0d88cf7891862bf1674694220a0473e29c81
rnjai <rnjailamba12@gmail.com>
2015-07-20 06:06:25 +0530

    Development email working

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,15 +66,19 @@ Rails.application.configure do
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
-  config.action_mailer.default_url_options = { :host => 'baywalkgoa.herokuapp.com' }
+
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'baywalkgoa.herokuapp.com' }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = {
-      :address              => 'smtp.gmail.com',
-      :port                 => 587,
-      :domain               => 'gmail.com',
-      :user_name            => 'rnjailamba@gmail.com',
-      :password             => '1!Srisathyasai',
-      :authentication       => 'login',
+      :address => "smtp.gmail.com",
+      :port => 587,
+      :domain => 'localhost:3000',
+      :user_name => "rnjailamba@gmail.com",
+      :password => "1!Srisathyasai",
+      :authentication => :plain,
       :enable_starttls_auto => true
   }
 
